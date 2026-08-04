28434f77a198ccb5c0bd8de9eb01c03e715991db
koushil24 <koushilr24@gmail.com>
2026-08-04 18:30:11 +0530

    Step 8.2 - Working Language Switch

--- js/script.js
+++ js/script.js
@@ -24,3 +24,60 @@ themeBtn.addEventListener("click", ()=>{
     }
 
 });
+const langBtn = document.getElementById("langBtn");
+
+let currentLanguage = localStorage.getItem("language") || "kn";
+
+const translations = {
+
+kn:{
+    "nav-home":"ಮುಖಪುಟ",
+    "nav-about":"ನಮ್ಮ ಬಗ್ಗೆ",
+    "nav-events":"ಕಾರ್ಯಕ್ರಮಗಳು",
+    "nav-team":"AGB ತಂಡ",
+    "nav-gallery":"ಗ್ಯಾಲರಿ",
+    "nav-news":"ಸುದ್ದಿ",
+    "nav-contact":"ಸಂಪರ್ಕಿಸಿ"
+},
+
+en:{
+    "nav-home":"Home",
+    "nav-about":"About",
+    "nav-events":"Events",
+    "nav-team":"AGB Team",
+    "nav-gallery":"Gallery",
+    "nav-news":"News",
+    "nav-contact":"Contact"
+}
+
+};
+
+function changeLanguage(language){
+
+    for(const id in translations[language]){
+
+        const element=document.getElementById(id);
+
+        if(element){
+
+            element.innerText=translations[language][id];
+
+        }
+
+    }
+
+    langBtn.innerText=language==="kn" ? "🌐 English" : "🌐 ಕನ್ನಡ";
+
+    localStorage.setItem("language",language);
+
+    currentLanguage=language;
+
+}
+
+changeLanguage(currentLanguage);
+
+langBtn.addEventListener("click",()=>{
+
+    changeLanguage(currentLanguage==="kn" ? "en" : "kn");
+
+});
