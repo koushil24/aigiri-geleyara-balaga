83c641aa4ac5602cb195e2e8efdd90dc37b4c927
koushil24 <koushilr24@gmail.com>
2026-08-04 16:37:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+// ===== Theme Toggle =====
 
+const themeBtn = document.getElementById("themeBtn");
+const body = document.body;
+
+// Load saved theme
+if(localStorage.getItem("theme") === "dark"){
+    body.classList.add("dark-mode");
+    themeBtn.textContent = "☀️";
+}else{
+    themeBtn.textContent = "🌙";
+}
+
+themeBtn.addEventListener("click", ()=>{
+
+    body.classList.toggle("dark-mode");
+
+    if(body.classList.contains("dark-mode")){
+        localStorage.setItem("theme","dark");
+        themeBtn.textContent="☀️";
+    }else{
+        localStorage.setItem("theme","light");
+        themeBtn.textContent="🌙";
+    }
+
+});
