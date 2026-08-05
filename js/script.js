91991af875db808cf71122e591380192d6d6dfb7
KOUSHIL R GOWDA <koushilr24@gmail.com>
2026-08-05 09:25:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -163,3 +163,20 @@ menuToggle.innerHTML="☰";
 });
 
 }
+// ===== Sticky Navbar =====
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 20){
+
+        navbar.classList.add("scrolled");
+
+    }else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
