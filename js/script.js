b925f3797251b750314c8b63e72eed59fa8ff09c
KOUSHIL R GOWDA <koushilr24@gmail.com>
2026-08-05 09:33:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -180,3 +180,32 @@ window.addEventListener("scroll", () => {
     }
 
 });
+// ===== Back To Top =====
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.addEventListener("click",()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+});
